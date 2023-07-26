a04308e3bdc250e0a695412159bf44f7feff1634
gabrdiias <127976037+gabrdiias@users.noreply.github.com>
2023-07-26 13:55:46 -0300

    Add files via upload

---
+++ aulinha.js
@@ -0,0 +1,80 @@
+//minha idade
+console.log("Tenho 16 anos")
+let idade = 16
+console.log(idade + 3.5)
+
+/*
+5
+*/
+
+
+console.log("Programei 1 semana nas férias")
+
+/*
+Esta programção simples, 
+imprime as informções indicadas após a função log
+*/
+
+let num = 6;
+let letNum = 8888888888888888888n;
+
+let gall = true
+let gaal = false
+
+const user = {
+    name: "Gabriel",
+    age: 16,
+    ba: true
+};
+
+let greeting = "Oi mano";
+let message = "Eae, tá dboa?";
+
+checkpoint = null;
+/*
+6
+*/
+let dataAtual = new Date(2023)
+let dataLua = new Date(1969)
+console.log(dataAtual - dataLua)
+/*
+7
+*/
+console.log(65/240)
+/*
+8
+*/
+console.log(0.2708*100)
+/*
+9
+*/
+console.log("Optmus"+"Prime")
+/*
+10
+*/
+console.log("Optmus"+" "+"Prime")
+/*
+11
+*/
+console.log("Optmus,Prime".length)
+/*
+12
+*/
+console.log("optmus prime".toUpperCase())
+/*
+13
+*/
+console.log(" Optmus Prime ".trim())
+/*
+14
+*/
+console.log(Math.random(1)* 100)
+/*
+15
+*/
+console.log(Math.ceil(36.9))
+/*
+16
+*/
+console.log(Number.isInteger(23))
+console.log(Number.isInteger(22.5))
